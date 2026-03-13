58e67cdc0c32389ed1bf0dba932873af7f8a2645
Lucas Crespo <lucas@every.to>
2026-03-13 07:21:40 +0000

    Disable drawShadow - library's built-in shadow was the dashed line

--- components/book-testimonial.tsx
+++ components/book-testimonial.tsx
@@ -148,7 +148,7 @@ export default function BookTestimonial() {
             width={340}
             height={480}
             showCover={true}
-            drawShadow={true}
+            drawShadow={false}
             flippingTime={800}
             usePortrait={false}
             startPage={0}
